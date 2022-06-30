7ae8fba9d6a00c0512b81c079bbbdd0cb9bde343
Catherine Tung <catt.tung@gmail.com>
2022-06-29 20:08:42 -0400

    Create handle image search function in image search with API call, pass as props to search form

--- src/components/ImageSearch.jsx
+++ src/components/ImageSearch.jsx
@@ -1,12 +1,23 @@
+import React, { useState } from 'react';
 
 import SearchForm from "./SearchForm";
 
+import { imageSearch } from '../services/api-calls';
 
 const ImageSearch = (props) => {
+  const [images, setImages] = useState([])
+
+  const handleImageSearch = formData => {
+    imageSearch(formData)
+    .then(imageHits => {
+      setImages(imageHits.hits)
+    })
+  }
+
   return (
     <>
       <h3>Image Search</h3>
-      <SearchForm />
+      <SearchForm handleImageSearch={handleImageSearch} />
     </>
   );
 }
