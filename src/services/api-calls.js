c51e48a76cc40063eca22b66f7866a7c8794c4f3
Catherine Tung <catt.tung@gmail.com>
2022-06-29 20:05:17 -0400

    API call function for Image Search

--- src/services/api-calls.js
+++ src/services/api-calls.js
@@ -1,2 +1,7 @@
 const baseUrl = 'https://pixabay.com/api/'
 const MY_KEY= process.env.REACT_APP_API_KEY_PIXABAY;
+
+export function imageSearch(formData) {
+  return fetch(`${baseUrl}?key=${MY_KEY}&q=${formData.query}`)
+  .then(res => res.json())
+}
